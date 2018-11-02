25e65d7eb215e5db29d7c6663c65648be32b7194
Lucas PARISOT <lucas-parisot@orange.fr>
2018-11-01 22:19:03 -0400

    gitignore et initialisation cordova

---
+++ app/src/main/java/ca/qc/cgmatane/informatique/myapplication/ActivitePrincipale.java
@@ -0,0 +1,13 @@
+package ca.qc.cgmatane.informatique.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivitePrincipale extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.vue_activite_principale);
+    }
+}
