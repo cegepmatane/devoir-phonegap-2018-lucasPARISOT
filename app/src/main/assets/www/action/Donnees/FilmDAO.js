66ba1594d6175b80e7b6c238150abcea3df5fe71
Lucas PARISOT <lucas-parisot@orange.fr>
2018-11-01 22:32:14 -0400

    Ajout de FilmDAO

---
+++ app/src/main/assets/www/action/Donnees/FilmDAO.js
@@ -0,0 +1,51 @@
+﻿var FilmDAO = function()
+{
+    var listeFilm;
+    var initialiser = function()
+    {
+        if(!listeFilm)
+        {
+            listeFilm = [];
+        }
+    }
+
+    this.chercherAvecId = function(id)
+    {
+
+    }
+
+    this.ajouter = function(cadeau)
+    {
+        // il faut ici attribuer un id au cadeau
+        if(listeFilm.length > 0)
+            film.id = listeFilm[listeFilm.length-1].id + 1;
+        else
+            film.id = 0;
+
+        listeFilm[film.id] = film;
+        localStorage['film'] = JSON.stringify(listeFilm);
+        console.log("JSON.stringify(listeFilm) : " + JSON.stringify(listeFilm));
+
+    }
+
+    this.lister = function()
+    {
+        console.log("lister->listeFilm : " + listeFilm);
+        console.log("lister->localStorage['film'] : " + localStorage['film']);
+        if(localStorage['film'])
+            listeFilm = JSON.parse(localStorage['film']);
+
+        for(position in listeFilm)
+        {
+
+            var film = new Film(listeFilm[position].nom,
+                                    listeFilm[position].realisateur,
+                                    listeFilm[position].description,
+                                    listeFilm[position].id);
+            listeFilm[position] = film;
+        }
+        return listeFilm;
+
+    }
+    initialiser();
+}
--- app/src/main/assets/www/action/application.js
+++ app/src/main/assets/www/action/application.js
@@ -1,1 +1,40 @@
-﻿
+﻿(function()
+{
+    var instance = this;
+
+    var initialiser = function()
+    {
+        this.cadeauDAO = new CadeauDAO();
+
+        window.addEventListener("hashchange",naviguer);
+
+        naviguer();
+
+    }
+
+    var naviguer = function()
+    {
+        var hash = window.location.hash;
+
+        if(!hash)
+        {
+            this.listeCadeauDonnee = this.cadeauDAO.lister();
+            var listeCadeauVue = new ListeCadeauVue(instance.listeCadeauDonnee);
+            listeCadeauVue.afficher();
+        }
+        else if(hash.match(/^#ajouter-cadeau/))
+        {
+            var ajouterCadeauVue = new AjouterCadeauVue(actionEnregistrerCadeau);
+            ajouterCadeauVue.afficher();
+        }
+        else
+        {
+            var navigation = hash.match(/^#cadeau\/([0-9]+)/);
+            //alert(navigation);
+            var idCadeau = navigation[1];
+            //alert(idCadeau);
+            var cadeauVue = new CadeauVue(instance.listeCadeauDonnee[idCadeau]);
+            cadeauVue.afficher();
+        }
+
+    }
